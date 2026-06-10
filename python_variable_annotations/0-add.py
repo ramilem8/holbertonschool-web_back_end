39a32c70d9f2616371e52e4e3aaced6ef0e88eb6
ramilem8 <13728@holbertonstudents.com>
2026-06-10 18:53:02 +0000

    add

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""Module that provides addition of two floats."""
+
 def add(a: float, b: float) -> float:
-    """Returns the sum of a and b."""
+    """Return the sum of a and b."""
     return a + b
