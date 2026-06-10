c994d7d329e9203a26355c81d53f78b18be9574e
ramilem8 <13728@holbertonstudents.com>
2026-06-10 19:02:47 +0000

    add

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that provides a floor function."""
+
+import math
+
+def floor(n: float) -> int:
+    """Return the floor of a float number."""
+    return math.floor(n)
