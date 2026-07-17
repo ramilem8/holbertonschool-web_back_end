7eb5b11406943c88709c59f8adb08ede1052cf9d
ramilem8 <13728@holbertonstudents.com>
2026-07-17 12:57:56 +0000

    add

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Insert a new document in a MongoDB collection."""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Insert a new document and return its _id."""
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
