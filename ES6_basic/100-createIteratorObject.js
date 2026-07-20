bd1d1406db8d64eab8cd2067ac65464fe89a5e58
ramilem8 <13728@holbertonstudents.com>
2026-07-20 15:45:27 +0000

    add

---
+++ ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+export default function createIteratorObject(report) {
+    const employees = [];
+  
+    for (const department of Object.values(report.allEmployees)) {
+      employees.push(...department);
+    }
+  
+    return employees;
+  }
