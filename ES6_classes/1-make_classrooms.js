7722bd4538aeece470cc54da9bb740d2dab9b0f3
ramilem8 <13728@holbertonstudents.com>
2026-07-21 08:08:05 +0000

    add

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+
+
+import Classroom from './o-classroom.js';
+export default function initializeRooms(){
+    return [ 
+        new Classroom(19),
+        new Classroom(20),
+        new classroom (34),
+    ];
+}
