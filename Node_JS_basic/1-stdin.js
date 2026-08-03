39834e5cdd829cae4a77dfe85561946c95943ac0
Your Name <you@example.com>
2026-08-03 11:15:56 +0000

    add

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,17 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data}`);
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
+});
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  process.exit();
 });
