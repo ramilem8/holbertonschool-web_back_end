e571b30ac23f954270ce1d1c51be2898c39b4473
ramilem8 <13728@holbertonstudents.com>
2026-08-03 09:36:52 +0000

    add

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,9 +1,5 @@
-/**
- * Prints a message to STDOUT.
- * @param {string} message - The message to display.
- */
-const displayMessage = (message) => {
+function displayMessage(message) {
     console.log(message);
-  };
+  }
   
   module.exports = displayMessage;
